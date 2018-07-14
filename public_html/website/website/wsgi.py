b5105f8edfc3b87c4c4765abd17dc74de064da8f
manuellouie <louieManuel238@gmail.com>
2018-07-14 17:15:33 -0600

    updated requirements.txt

--- public_html/website/website/wsgi.py
+++ public_html/website/website/wsgi.py
@@ -16,3 +16,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+application.add_files('/personal/static', prefix='static/')
