9468a26519cdac1ae385f26d441ad23b5b9b2472
manuellouie <louieManuel238@gmail.com>
2018-07-14 11:26:26 -0600

    static files Not working

---
+++ public_html/website/staticfiles/personal/materialize/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.project-img').parallax();
+    $('.modal').modal();
+    $('.slider').slider();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+ 
