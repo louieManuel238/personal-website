26b67f34d857485a18078ebeeb41c1d679139a0c
manuellouie <louieManuel238@gmail.com>
2018-07-30 13:20:23 -0600

    update admin page

--- public_html/website/personal/admin.py
+++ public_html/website/personal/admin.py
@@ -2,19 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 
-from personal.models import Project, Blog, Profile
+from personal.models import Project, Blog, Profile, ProjectImgs
 
 class ProjectAdmin(admin.ModelAdmin):
-	pass
+    pass
 
 admin.site.register(Project, ProjectAdmin)
 
 class BlogAdmin(admin.ModelAdmin):
-	pass
+    pass
 
 admin.site.register(Blog, BlogAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
-        pass
+    pass
 
 admin.site.register(Profile, ProfileAdmin)
+
+class ProjectImgsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ProjectImgs, ProjectImgsAdmin)
